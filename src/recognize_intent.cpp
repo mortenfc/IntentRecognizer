49ca9b6e1c949943931640bcc1d3579096c7a1d6
Morten <morten.christensen@sensible4.fi>
2021-04-17 15:53:04 +0300

    remove commented out code

--- src/recognize_intent.cpp
+++ src/recognize_intent.cpp
@@ -7,12 +7,6 @@
 #include <string>
 #include <iostream>
 
-// * What is the weather like today? => Prints (Intent: Get Weather)
-// * What is the weather like in Paris today? => Prints (Intent: Get Weather City)
-// * What is the weather like in New York today? => Prints (Intent: Get Weather City)
-// * Am I free at 13:00 PM tomorrow? ==> Prints (Intent: Check calendar)
-// * Tell me an interesting fact. => Prints (Intent: Get Fact)
-
 std::string recognizeIntent(const std::string& input_line)
 {
     std::vector<std::string> input_words;
@@ -32,36 +26,14 @@ std::string recognizeIntent(const std::string& input_line)
     {
         for (size_t j = 0; j < weather.array_size; j++)
         {
-            // std::vector<std::string> weather_words;
-            // boost::split(weather_words, weather.weather, [](char c) { return c == ' '; });
-            // std::cout << "weather_word: " << weather_word << "\n";
             is_weather_match = is_weather_match | areWordsSimilar(input_words[i], weather.weather[j]);
             is_city_match = is_city_match | areWordsSimilar(input_words[i], weather.city[j]);
-            // std::cout << "input_words[i]: " << input_words[i] << "\n";
-            // std::cout << "weather.weather[j]: " << weather.weather[j] << "\n";
-            // std::cout << "weather.city[j]: " << weather.city[j] << "\n";
-            // std::cout << "is_weather_match: " << is_weather_match << "\n";
-            // std::cout << "is_city_match: " << is_city_match << "\n";
             if (is_weather_match + is_city_match < 2 and i < input_words_size - 1)
             {
-                // std::cout << "i " << i << " | j " << j << "\n";
-                // std::cout << "input_words[i] | input_words[i + 1]: " << input_words[i] << " | " << input_words[i + 1]
-                //           << "\n";
-                // std::cout << "weather.weather[j]: " << weather.weather[j] << "\n";
                 is_weather_match =
                     is_weather_match | areWordsSimilar(input_words[i] + input_words[i + 1], weather.weather[j]);
-                is_city_match =
-                    is_city_match | areWordsSimilar(input_words[i] + input_words[i + 1], weather.city[j]);
-                // std::cout << "is_weather_match: " << is_weather_match << "\n";
-                // std::cout << "is_city_match: " << is_city_match << "\n";
+                is_city_match = is_city_match | areWordsSimilar(input_words[i] + input_words[i + 1], weather.city[j]);
             }
-            // else
-            // {
-            //     std::cout << "input_words[i]: " << input_words[i] << "\n";
-            //     std::cout << "weather.weather[j]: " << weather.weather[j] << "\n";
-            //     std::cout << "is_weather_match: " << is_weather_match << "\n";
-            //     std::cout << "is_city_match: " << is_city_match << "\n";
-            // }
         }
     }
     type_match_count = uint(is_weather_match) + uint(is_city_match);
--- tests/recognize_intent_tests.cpp
+++ tests/recognize_intent_tests.cpp
@@ -6,16 +6,6 @@
 
 TEST(recognizeIntent, basic_line_input)
 {
-    // std::string input_line = "Am I free at 13:00 PM tomorrow?";
-    // std::string input_line = "What is the weather like in New York today?";
-    // std::string input_line = "What is the weather like in Paris today?";
-    // std::string input_line = "What is the weather like today?";
-    // * What is the weather like today? => Prints (Intent: Get Weather)
-    // * What is the weather like in Paris today? => Prints (Intent: Get Weather City)
-    // * What is the weather like in New York today? => Prints (Intent: Get Weather City)
-    // * Am I free at 13:00 PM tomorrow? ==> Prints (Intent: Check calendar)
-    // * Tell me an interesting fact. => Prints (Intent: Get Fact)
-
     std::string input_line = "What is the weather like today?";
     std::string output = recognizeIntent(input_line);
     ASSERT_EQ(output, "Intent: Get Weather") << "With input line: \"" + input_line + "\"";
